d0679669cd3c7fdb29439fa338236d9ac892ed46
Ryan Joya <ryanjoya@users.noreply.github.com>
2015-04-04 20:50:09 -0500

    Create 10.7 Investing in Stock.py

---
+++ 10.7 Investing in Stock.py
@@ -0,0 +1,17 @@
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+
+print prices
+
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+
+print stock
